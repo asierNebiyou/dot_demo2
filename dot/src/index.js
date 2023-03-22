1213b0bafcfdc9e5d69ed1fd522f0972615304ef
asierNebiyou <yeabsra143@gmail.com>
2023-03-22 13:42:53 +0300

    ‘commited2’

--- dot/src/index.js
+++ dot/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
 import './index.css';
+import useExternalScripts from "./hooks/useExternalScripts"
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),useExternalScripts("../public/bootstrap/js/bootstrap.bundle.js")
 );
